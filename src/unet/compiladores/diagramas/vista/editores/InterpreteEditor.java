ff7d095676680e915378550c6861203ceadad172
Anthonny Crespo <anthonny.crespo@gmail.com>
2011-07-08 15:29:48 -0430

    -.-

--- src/unet/compiladores/diagramas/vista/editores/InterpreteEditor.java
+++ src/unet/compiladores/diagramas/vista/editores/InterpreteEditor.java
@@ -91,6 +91,8 @@ public class InterpreteEditor extends javax.swing.JDialog {
             figuraModelo = null;
         }
     }
+    
+    
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         
