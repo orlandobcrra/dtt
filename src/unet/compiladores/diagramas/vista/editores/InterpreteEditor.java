298bd2b319c903ac150cd1b166c143d303cef5ff
Anthonny Crespo <anthonny.crespo@gmail.com>
2011-07-09 00:08:09 -0430

    -.-

--- src/unet/compiladores/diagramas/vista/editores/InterpreteEditor.java
+++ src/unet/compiladores/diagramas/vista/editores/InterpreteEditor.java
@@ -93,6 +93,7 @@ public class InterpreteEditor extends javax.swing.JDialog {
     }
     
     
+    
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         
